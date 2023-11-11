228714eff0b6d25091c9348bdb09c6c48f03df01
Anjali Mavingal <anjalimavingal@gmail.com>
2023-11-11 11:50:09 +0530

    crud initaial commit

--- src/app/shared/employees/employees.component.ts
+++ src/app/shared/employees/employees.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
+import { Employee } from '../models/employee';
 import { AddEditEmployeeComponent } from './add-edit-employee/add-edit-employee.component';
 
 @Component({
@@ -6,11 +8,38 @@ import { AddEditEmployeeComponent } from './add-edit-employee/add-edit-employee.
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css'],
 })
-export class EmployeesComponent {
+export class EmployeesComponent implements OnInit {
   @ViewChild('addeditemployee')
   modal: AddEditEmployeeComponent;
+  employees: Employee[];
+  constructor(private _emplyee: EmployeeService) {}
+
+  ngOnInit() {
+    this._emplyee.getEmployees().subscribe({
+      next: (data) => {
+        this.employees = data;
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
+  }
 
   openModal() {
     this.modal.openModal();
+  }
+
+  deleteEmployee(id: number) {
+    this._emplyee.deleteEmployee(id + 1).subscribe({
+      next: () => {
+        alert('Deleted Employee');
+      },
+      error: console.log,
+    });
+  }
+
+  editEmployee(id: number) {
+    this.modal.openModal();
+    this.modal.setInputData(id + 1);
   }
 }
